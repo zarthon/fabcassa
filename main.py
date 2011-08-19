7104d953f1808f79384c37a8b00ff8750351ecfc
Mohit Kothari <mohitrajkothari@gmail.com>
2011-08-20 00:16:47 +0530

    Will not allow updation of exisiting username

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ USERNAME = None
 COL_USERS = ['id', 'username', 'password']
 COL_USERNA = ['id']
 
+#Initialise Connections
 def init():
     global KEYSPACE, USERS, USERNAME
     try:
@@ -34,22 +35,33 @@ def init():
         print sys.exc_info()
         sys.exit()
 
+#Insert a new username if not present
 def insert_new():
     global USERS, USERNAME
-    username = str(raw_input("Enter new Username: "))
-    password = getpass.getpass("Enter new password")
-    password2 = getpass.getpass("Re-Enter new password")
-    if password != password2:
-        print "Passwords dont match"
-    elif USERS is None or USERNAME is None:
+    
+    if USERS is None or USERNAME is None:
         print "Not connected to Column Family"
         sys.exit()
-    else:
+    try:
+        username = str(raw_input("Enter new Username: "))
+#Check if username already present
         try:
+            if USERNAME.get(username):
+                print "Username already exists"
+                return 
+        except:
+            pass
+
+        password = getpass.getpass("Enter new password: ")
+        password2 = getpass.getpass("Re-Enter new password: ")
+        if password != password2:
+            print "Passwords dont match"
+        else:
             user_id = str(uuid.uuid4())
             USERS.insert(user_id,{COL_USERS[0]:user_id, COL_USERS[1]:username, COL_USERS[2]:password})
             USERNAME.insert(username,{COL_USERNA[0]:user_id})
-        except:
+            print "User with username ",username," successfully created"
+    except:
             print sys.exc_info()
 if __name__ == "__main__":
     init()
